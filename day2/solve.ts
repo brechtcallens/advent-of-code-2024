1c439a53d5227d61c30429fb350e9d047cf2aec2
Brecht Callens <brecht.callens@ericsson.com>
2024-12-02 19:24:44 +0100

    Solved day2

---
+++ day2/solve.ts
@@ -0,0 +1,70 @@
+import fs from "fs";
+
+const parse = (fileName: string) => {
+  const input = fs.readFileSync(fileName, "utf8");
+
+  const lines = input.split("\n");
+  const grid = lines.map((line) =>
+    line
+      .trim()
+      .split(" ")
+      .map((level) => parseInt(level))
+  );
+  return grid;
+};
+
+const checkLine = (line: number[]) => {
+  const incr = line[0] < line[1];
+  let prevValue;
+  for (let value of line) {
+    if (prevValue) {
+      const diff = value - prevValue;
+      if (
+        (incr && (diff < 1 || diff > 3)) ||
+        (!incr && (diff > -1 || diff < -3))
+      ) {
+        return false;
+      }
+    }
+    prevValue = value;
+  }
+  return true;
+};
+
+const solve1 = (fileName: string) => {
+  const lines = parse(fileName);
+
+  const safeness = lines.map(checkLine);
+
+  return safeness.reduce((acc, v) => acc + (v ? 1 : 0), 0);
+};
+
+const solve2 = (fileName: string) => {
+  const lines = parse(fileName);
+
+  const safeness = lines.map((line) => {
+    const options = line.map((_, index) => [
+      ...line.slice(0, index),
+      ...line.slice(index + 1, line.length),
+    ]);
+    return options.some(checkLine);
+  });
+
+  return safeness.reduce((acc, v) => acc + (v ? 1 : 0), 0);
+};
+
+const main = (runExampleInput: boolean) => {
+  const folderPath = import.meta.url
+    .replace("file://", "")
+    .replace("/solve.ts", "");
+  const filename = `${folderPath}/${
+    runExampleInput ? "example_input" : "input"
+  }`;
+  console.log(`Solving ${runExampleInput ? "example" : "real"} input`);
+  console.log(`Task 1: ${solve1(filename)}`);
+  console.log(`Task 2: ${solve2(filename)}`);
+};
+
+// Run!
+const RUN_EXAMPLE_INPUT = process.argv[2] === "example";
+main(RUN_EXAMPLE_INPUT);
