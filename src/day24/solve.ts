a59c39030a0af9accfe58271c705be94c648fda7
Brecht Callens <brecht.callens@ericsson.com>
2024-12-28 09:15:01 +0100

    Solved day24 part 2

--- src/day24/solve.ts
+++ src/day24/solve.ts
@@ -4,13 +4,30 @@ const parse = (fileName: string) => {
   const input = fs.readFileSync(fileName, "utf8");
 
   const [_values, _gates] = input.split("\n\n");
-  const values: Record<string, number> = _values
+
+  const valuesSplitted = _values
     .split("\n")
-    .map((line) => line.trim().split(": "))
-    .reduce((acc, splitLine) => {
+    .map((line) => line.trim().split(": "));
+
+  const values: Record<string, number> = valuesSplitted.reduce(
+    (acc, splitLine) => {
       acc[splitLine[0]] = parseInt(splitLine[1]);
       return acc;
-    }, {});
+    },
+    {}
+  );
+
+  const xString = valuesSplitted
+    .filter((value) => value[0].startsWith("x"))
+    .map((value) => value[1])
+    .reverse()
+    .join("");
+
+  const yString = valuesSplitted
+    .filter((value) => value[0].startsWith("y"))
+    .map((value) => value[1])
+    .reverse()
+    .join("");
 
   const gates: Record<string, string[]> = _gates
     .split("\n")
@@ -25,10 +42,10 @@ const parse = (fileName: string) => {
     .filter((target) => target.startsWith("z"))
     .sort();
 
-  return { values, gates, targets };
+  return { values, gates, targets, xString, yString };
 };
 
-const operators = {
+const operators: Record<string, (a: number, b: number) => number> = {
   XOR: (a: number, b: number) => a ^ b,
   OR: (a: number, b: number) => a | b,
   AND: (a: number, b: number) => a & b,
@@ -53,6 +70,30 @@ const solveRecursive = (
   return targetResult;
 };
 
+const equationOperators: Record<string, (a: string, b: string) => string> = {
+  XOR: (a, b) => (a < b ? `(${a} XOR ${b})` : `(${b} XOR ${a})`),
+  OR: (a, b) => (a < b ? `(${a} OR ${b})` : `(${b} OR ${a})`),
+  AND: (a, b) => (a < b ? `(${a} AND ${b})` : `(${b} AND ${a})`),
+};
+
+const constructEquation = (
+  target: string,
+  values: Record<string, number>,
+  gates: Record<string, string[]>
+) => {
+  if (target in values) {
+    return target;
+  }
+
+  const [a, op, b] = gates[target];
+  const equation = equationOperators[op](
+    constructEquation(a, values, gates),
+    constructEquation(b, values, gates)
+  );
+
+  return equation;
+};
+
 const solve1 = (fileName: string) => {
   const { values, gates, targets } = parse(fileName);
   const resultString = targets
@@ -64,9 +105,24 @@ const solve1 = (fileName: string) => {
 };
 
 const solve2 = (fileName: string) => {
-  const { values, gates } = parse(fileName);
-  // Solve!
-  console.log(Object.keys(gates).length);
+  const { values, gates, targets, xString, yString } = parse(fileName);
+
+  const resultString = targets
+    .reverse()
+    .map((target) => solveRecursive(target, values, gates))
+    .join("");
+  const result = parseInt(resultString, 2);
+
+  console.log(xString.padStart(46, " "), parseInt(xString, 2));
+  console.log(yString.padStart(46, " "), parseInt(yString, 2));
+  console.log(resultString.padStart(46, " "), parseInt(resultString, 2));
+  console.log(parseInt(xString, 2) + parseInt(yString, 2));
+
+  for (const target of targets) {
+    const targetResult = constructEquation(target, values, gates);
+    // console.log(target, targetResult.length, targetResult);
+  }
+
   return -1;
 };
 
